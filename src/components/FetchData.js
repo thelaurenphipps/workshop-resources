d5e15b3cd58eb6e273f1b67527519598e5f37cbc
req91236 <lauren.phipps@mutualofomaha.com>
2020-03-02 15:03:44 -0600

    starting to think through lab 4

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,3 +1,51 @@
 import axios from 'axios'
 
-export default {}
+export default {
+    props: {
+        url: {
+            type: String
+        }
+    },
+    data() {
+      return {
+          data: null,
+          isPending: false,
+          error: null
+      }
+    },
+    methods: {
+        fetch () {
+            this.isPending = true;
+            axios.get(this.url)
+                .then(response => {
+                    this.error = null;
+                    this.data = response.data;
+                    this.isPending = false;
+                })
+                .catch(error => {
+                    this.error = error;
+                    this.isPending = false;
+                })
+        }
+    },
+    render() {
+        if (this.isPending) {
+            return this.$scopedSlots.loading({
+                isPending: this.isPending
+            })
+        }
+        else if (this.error) {
+            return this.$scopedSlots.error({
+                error: this.error,
+                refresh: this.fetch
+            })
+        }
+        else {
+            return this.$scopedSlots.default({
+                data: this.data,
+                refresh: this.fetch
+            })
+        }
+
+    }
+}
